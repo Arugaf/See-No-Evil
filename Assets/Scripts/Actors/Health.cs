46870181b8de26af0ad30a5d073e527e17954b65
Arugaf <arug113@gmail.com>
2025-06-13 01:27:56 +0300

    feature: health for all actors & health bar

---
+++ Assets/Scripts/Actors/Health.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Actors {
+    public enum Status {
+        Alive,
+        Dead,
+    }
+
+    public class Health : MonoBehaviour {
+        public uint initialHealth = 100;
+        public uint maxHealth = 100;
+        public int health;
+
+        [SerializeField] public Status status = Status.Alive;
+
+        public UnityEvent gotHealthIsZero;
+
+        private void Start() {
+            health = (int)initialHealth;
+        }
+
+        private void Update() {
+            // todo: remove from release version
+            ApplyHpChange(0);
+        }
+
+        public void ApplyHpChange(int hpChange) {
+            health -= hpChange;
+            status = health <= 0 ? Status.Dead : Status.Alive;
+
+            if (status == Status.Dead) {
+                gotHealthIsZero?.Invoke();
+            }
+        }
+    }
+}
