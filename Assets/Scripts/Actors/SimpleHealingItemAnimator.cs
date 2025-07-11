8e965f3eb58a7d91eeb5397694a28b328186ca68
VisualClony <mrcoolmoder@gmail.com>
2025-07-11 22:47:44 +0300

    Integrated Jam and Cocktail Monster (partially, i better do some cleanup for him later)

---
+++ Assets/Scripts/Actors/SimpleHealingItemAnimator.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+
+public class SimpleHealingItemAnimator : MonoBehaviour
+{
+    [SerializeField] private Vector3 verticalAcc;
+    [SerializeField] private float period;
+    [SerializeField] private float rotationPeriod;
+        [SerializeField] private float rotationAmplitude;
+    [SerializeField] private Transform targetTransorm;
+
+    private bool isVisible = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isVisible)
+        {
+            float x = Mathf.Sin(Time.time * period);
+            float rot = Mathf.Sin(Time.time * rotationPeriod);
+            targetTransorm.localPosition = x * verticalAcc;
+            targetTransorm.localEulerAngles = new Vector3(0, rot * rotationAmplitude, 0);
+        }
+    }
+    void OnBecameVisible()
+    {
+        isVisible = true;
+    }
+    void OnBecameInvisible()
+    {
+        isVisible = false;        
+    }
+}
