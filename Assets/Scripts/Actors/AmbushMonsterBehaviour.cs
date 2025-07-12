e472605020a46e6668ae35fe614039405b1f8b4e
VisualClony <mrcoolmoder@gmail.com>
2025-07-12 18:08:41 +0300

    Ambush Logic monster added >:} it's like screamer and you can only dodge it

---
+++ Assets/Scripts/Actors/AmbushMonsterBehaviour.cs
@@ -0,0 +1,49 @@
+using Cysharp.Threading.Tasks;
+using Features.VFX;
+using UnityEngine;
+
+public abstract class AbstractAmbushMonsterBehaviour: MonoBehaviour
+{
+    public abstract UniTask DoAmbush(Transform player);
+}
+public class AmbushMonsterBehaviour : AbstractAmbushMonsterBehaviour
+{
+    [SerializeField] private Transform[] targetPoints;
+    [SerializeField] private float speed;
+    [SerializeField] private float rotationSmoothSpeed;
+    [SerializeField] private float velocitySmoothTime = 0.2f;
+    [SerializeField] private float critDistance = 0.2f;
+    public override async UniTask DoAmbush(Transform target)
+    {
+        gameObject.SetActive(true);
+        transform.position = targetPoints[0].position;
+        int i = 0;
+        Vector3 acc = Vector3.zero, velocity = Vector3.zero;
+        while (i < targetPoints.Length)
+        {
+            Debug.Log("DOING");
+            float tFactor = Mathf.Exp(rotationSmoothSpeed * Time.deltaTime) - 1;
+            Vector3 moveVector = targetPoints[i].position - transform.position;
+            Quaternion rot = Quaternion.LookRotation(moveVector.normalized, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rot, tFactor);
+            Vector3 targetVelocity = moveVector.normalized * speed;
+            velocity = Vector3.SmoothDamp(velocity, targetVelocity, ref acc, velocitySmoothTime);
+            transform.position = transform.position + velocity * Time.deltaTime;
+            await UniTask.WaitForEndOfFrame();
+            if (moveVector.magnitude < critDistance) i++;
+        }
+        gameObject.SetActive(false);
+    }
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.coral;
+
+        for (var i = 0; i < targetPoints.Length - 1; ++i)
+        {
+            Gizmos.DrawLine(targetPoints[i].position, targetPoints[i + 1].position);
+        }
+
+    }
+#endif
+}
---
+++ Assets/Scripts/Actors/AmbushMonsterTrigger.cs
@@ -0,0 +1,49 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+namespace Gameplay
+{
+    [RequireComponent(typeof(Collider))]
+    public class AmbushMonsterTrigger : MonoBehaviour
+    {
+        private bool canTrigger = true;
+        [SerializeField] private float determTag = 0.3f;
+        [SerializeField] private float cooldown = 10.0f;
+        [SerializeField] private GameObject activateObject;
+        [SerializeField] private AbstractAmbushMonsterBehaviour ambushBehaviour;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!canTrigger) return;
+            if (other.CompareTag("Player"))
+            {
+                canTrigger = false;
+                TriggerProcess(other.transform).Forget();
+            }
+        }
+        private async UniTask TriggerProcess(Transform player)
+        {
+            Vector3 startPos = player.position;
+            await UniTask.WaitForSeconds(determTag);
+            Vector3 forwarder = player.position - startPos;
+            float perform = Vector3.Dot(forwarder, transform.forward);
+            if (perform <= 0)
+            {
+                canTrigger = true;
+                return;
+            }
+            else
+            {
+                Debug.Log("DO AMBUSH");
+                activateObject.SetActive(true);
+                await ambushBehaviour.DoAmbush(player);
+                activateObject.SetActive(false);
+                canTrigger = true;
+            }
+        }
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Debug.DrawRay(transform.position, transform.forward * 2, Color.yellow);
+        }
+#endif
+    }
+}
