861beb30e494c75b974a948a268f0ee638b2c7c1
VisualClony <mrcoolmoder@gmail.com>
2025-08-05 16:03:15 +0300

    There are no groups now but LABELS. If you want to "optimize" it for Windows/Android just enable "Pack Together By Label"

--- Assets/Scripts/EntryPoint.cs
+++ Assets/Scripts/EntryPoint.cs
@@ -12,22 +12,12 @@ public interface IFirmwareLoadScreen
 public class EntryPoint : MonoBehaviour
 {
     public AssetReferenceGameObject firmware;
-    public List<AssetReference> webglPreloadAssets;
     private IFirmwareLoadScreen screen;
     public async Awaitable Start()
     {
         screen = CoreInstaller.GetFirmwareLoadScreen();
         Debug.Log("GAME INIT...");
         screen.Enabled = true;
-        // I REALLY don't want the GAME to be broken later because of a bad internet
-        // So I load everything at the start, and never release it.
-        // Yes, this is a bad approach, but our game isn't too big to take this into account.
-#if UNITY_WEBGL
-        foreach (var reference in webglPreloadAssets)
-        {
-            await ProgressBarUpdator(Addressables.LoadResourceLocationsAsync(reference), screen);
-        }
-#endif
         GameObject x = await ProgressBarUpdator(firmware.LoadAssetAsync(), screen);
         var instance = Instantiate(x);
         LifetimeScope.EnqueueParent(instance.GetComponent<LifetimeScope>());
